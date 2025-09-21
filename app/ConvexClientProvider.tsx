602c1d62a0897416692075d7f210801309bcf76d
pax2678 <pax.prakarsa@gmail.com>
2025-09-20 20:28:35 -0700

    clean: add check for NEXT_PUBLIC_CONVEX_URL environment var before using it

--- app/ConvexClientProvider.tsx
+++ app/ConvexClientProvider.tsx
@@ -5,6 +5,10 @@ import { ClerkProvider, useAuth } from "@clerk/nextjs";
 import { ConvexReactClient } from "convex/react";
 import { ReactNode } from "react";
 
+if (!process.env.NEXT_PUBLIC_CONVEX_URL) {
+  throw new Error('Missing NEXT_PUBLIC_CONVEX_URL in your .env file')
+}
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export function ConvexClientProvider({ children }: { children: ReactNode }) {
